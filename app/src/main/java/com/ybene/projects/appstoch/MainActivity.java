6084a19d97a703c292eb44be7d8c3e76c25b85b2
BENETREAULT <beetle.buzzy@gmail.com>
2019-10-15 10:13:24 +0200

    Début de l'interface graphique

--- app/src/main/java/com/ybene/projects/appstoch/MainActivity.java
+++ app/src/main/java/com/ybene/projects/appstoch/MainActivity.java
@@ -2,13 +2,29 @@ package com.ybene.projects.appstoch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageButton imgbStart, imgbAbout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imgbStart = findViewById(R.id.activity_main_start_button);
+        imgbAbout = findViewById(R.id.activity_main_about);
+
+        imgbStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentStoch = new Intent(MainActivity.this, StochActivity.class);
+                startActivity(intentStoch);
+            }
+        });
     }
 }
