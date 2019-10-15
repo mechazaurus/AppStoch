d16e34bb5f82f8250d12db1049cd504030824bde
lucien-le-guellec <lucien.leguellec@etu.univ-tours.fr>
2019-10-15 09:10:43 +0200

    création classe calculfile

--- app/src/main/java/com/ybene/projects/appstoch/calcul/CalculFile.java
+++ app/src/main/java/com/ybene/projects/appstoch/calcul/CalculFile.java
@@ -1,4 +1,39 @@
 package com.ybene.projects.appstoch.calcul;
 
 public class CalculFile {
+    private double lambda;
+    private double mu;
+    private int s;
+    private int k;
+
+    public CalculFile(double lambda, double mu, int s, int k) {
+        this.lambda = lambda;
+        this.mu = mu;
+        this.s = s;
+        this.k = k;
+    }
+
+    private double q0;
+    private double w;
+    private double l;
+
+    public void compute(){
+        if (s>1) computeMMS();
+        else if (k>0) computeMM1k();
+        else computeMM1();
+    }
+
+    private void computeMM1(){
+
+    }
+
+    private void computeMM1k(){
+
+    }
+
+    private void computeMMS(){
+
+    }
+
+
 }
